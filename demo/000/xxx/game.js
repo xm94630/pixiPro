76a6152a81ee840d796cd2f04c65fd17856a13ff
91f_100006008444_xm946300 <91f_100006008444_xm946300@git.cloud.tencent.com>
2018-09-02 20:47:44 +0800

    简单全屏案例

--- demo/000/xxx/game.js
+++
@@ -1,9 +0,0 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Couchfriends
- * www.couchfriends.com
- *
- * This file is used automatically include all js files in the build en src.
- */
-Game.init();
